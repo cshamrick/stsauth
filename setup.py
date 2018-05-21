e9591798c6e87b7b4589246e42cceef255148497
Hamrick, Scott <scott.hamrick@bbva.com>
2018-05-21 14:58:57 -0500

    Fixed dependency versions.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="stsauth",
-    version='0.1.1',
+    version='0.1.2',
     py_modules=['stsauth', 'cli'],
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -13,13 +13,13 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     install_requires=[
-        'awscli==1.15.24',
-        'boto3==1.7.24',
-        'click==6.7',
-        'configparser==3.5.0',
-        'requests==2.18.4',
-        'requests-ntlm==1.1.0',
-        'bs4==0.0.1'
+        'awscli>=1.15.24',
+        'boto3>=1.7.24',
+        'click>=6.7',
+        'configparser>=3.5.0',
+        'requests>=2.18.4',
+        'requests-ntlm>=1.1.0',
+        'bs4>=0.0.1'
     ],
     entry_points='''
         [console_scripts]
