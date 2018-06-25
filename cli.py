0821b1a37b0b5179b94598293eae07d2cf903222
phillipjf <phillipjf@users.noreply.github.com>
2018-06-25 15:48:40 -0500

    Add support for reading/writing to named profiles

--- cli.py
+++ cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import re
 import sys
 
 import click
@@ -21,24 +22,26 @@ click_log.basic_config(logger)
 @click.option('--domain', '-d', help='The active directory domain.')
 @click.option('--credentialsfile', '-c', help='Path to AWS credentials file.',
               default='~/.aws/credentials')
+@click.option('--profile', '-l', help='Name of config profile.', default='saml')
 @click.option('--region', '-r', default=None, help='The AWS region to use. ex: us-east-1')
 @click.option('--output', '-o', default=None, type=click.Choice(['json', 'text', 'table']))
 @click.option('--force', '-f', is_flag=True, help='Auto-accept confirmation prompts.')
 @click.version_option('--version', '-V')
 @click_log.simple_verbosity_option(logger)
 def cli(username, password, idpentryurl, domain,
-        credentialsfile, region, output, force):
+        credentialsfile, profile, region, output, force):
     # UNSET any proxy vars that exist in the session
     unset_proxy()
 
     sts_auth = STSAuth(username, password, credentialsfile,
-                       idpentryurl, domain, region, output)
+                       idpentryurl, profile, domain, region, output)
 
     if not sts_auth.config_file_is_valid:
         sys.exit(1)
 
     if not sts_auth.credentials_expired and not force:
-        if not click.confirm('Credentials still valid, would you like to continue?'):
+        msg = 'Credentials for profile {!r} valid, would you like to continue?'
+        if not click.confirm(msg.format(sts_auth.profile)):
             sys.exit(0)
 
     sts_auth.parse_config_file()
@@ -60,8 +63,10 @@ def cli(username, password, idpentryurl, domain,
     # Use the assertion to get an AWS STS token using Assume Role with SAML
     token = sts_auth.fetch_aws_sts_token(role_arn, principal_arn, assertion)
 
-    # Put the credentials into a saml specific section
-    sts_auth.write_saml_conf(token)
+
+    # Put the credentials into a role specific section
+    role_for_section = parse_role_for_profile(role_arn)
+    sts_auth.write_saml_conf(token, role_for_section)
 
     # Give the user some basic info as to what has just happened
     msg = (
@@ -71,10 +76,11 @@ def cli(username, password, idpentryurl, domain,
         'Note that it will expire at {expiry}.\n'
         'After this time, you may safely rerun this script to refresh your access key pair.\n'
         'To use this credential, call the AWS CLI with the --profile option '
-        '(e.g. aws --profile saml ec2 describe-instances).\n'
+        '(e.g. aws --profile {role} ec2 describe-instances).\n'
         '--------------------------------------------------------------\n'
         .format(config_file=sts_auth.credentialsfile,
-                expiry=token.get('Credentials', {}).get('Expiration', ''))
+                expiry=token.get('Credentials', {}).get('Expiration', ''),
+                role=role_for_section)
     )
     click.secho(msg, fg='green')
 
@@ -115,3 +121,17 @@ def unset_proxy():
         if var in os.environ:
             logger.debug('Unsetting {!r} environment variable!'.format(var))
             del os.environ[var]
+
+def parse_role_for_profile(role):
+    account_re = re.compile(r'::(\d+):')
+    account_id = '000000000000'
+    role_name = 'Unknown-Role-Name'
+
+    _account_id = re.search(account_re, role)
+    _role_name = role.split('/')
+    if _account_id.groups():
+        account_id = _account_id.groups()[0]
+    if len(_role_name) == 2:
+        role_name = _role_name[1]
+
+    return '{}-{}'.format(account_id, role_name)
