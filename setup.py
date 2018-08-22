f7900fc8e64db98828a792186e320e2bdb60eb4f
phillipjf <phillipjf@users.noreply.github.com>
2018-08-22 12:16:18 -0500

    Bump version, changelog

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ tests_require = get_requirements('test')
 
 setup(
     name='stsauth',
-    version='0.2.2',
+    version='0.2.3',
     author='Scott Hamrick',
     author_email='scott@scotthamrick.com',
     description='CLI tool for fetching AWS tokens.',
