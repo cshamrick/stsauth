c9cb4f68582683dba29b74366087d27de323afa8
phillipjf <phillipjf@users.noreply.github.com>
2018-08-22 12:14:44 -0500

    Use full path to load requirements files in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
+import os
 from setuptools import setup
 
 
 def get_requirements(env=''):
+    path = os.path.dirname(os.path.abspath(__file__))
     fn = 'requirements{}{}.txt'.format(('-' if env else ''), env)
-    with open(fn) as fp:
+    with open(os.path.join(path, fn)) as fp:
         return [x.strip() for x in fp.read().split('\n') if not x.startswith('#')]
 
 
