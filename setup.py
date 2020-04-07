4f8267daff70cf9cb60d116a12f7e7725e96b35d
Phillip Ferentinos <phillipjf@users.noreply.github.com>
2020-04-07 10:24:55 -0500

    Bumps Version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ tests_require = get_requirements('test')
 
 setup(
     name='stsauth',
-    version='0.5.1',
+    version='0.5.2',
     author='Scott Hamrick',
     author_email='scott@scotthamrick.com',
     description='CLI tool for fetching AWS tokens.',
