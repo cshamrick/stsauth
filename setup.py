57b0a283af8f34c2652cca4af2643e1dfee19a72
Phillip Ferentinos <phillipjf@users.noreply.github.com>
2021-07-27 09:19:50 -0500

    build(setup.py): remove python 2 classifiers
    BREAKING CHANGE:

--- setup.py
+++ setup.py
@@ -39,8 +39,6 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
